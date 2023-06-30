6bd6a805e5616cf9b508a5f94b9c26f484063ca4
Chau Nhi <chaunhihue@gmail.com>
2023-06-30 13:06:03 +0700

    upload source code

---
+++ js/tabs.js
@@ -0,0 +1,23 @@
+function tabs(evt, categoryName) {
+  let i, tabcontent, tablinks;
+
+  tabcontent = document.getElementsByClassName("tabcontent");
+
+  for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
+  }
+
+  tablinks = document.getElementsByClassName("tablinks");
+
+  for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].className = tablinks[i].className.replace("active", "");
+  }
+
+  document.getElementById(categoryName).style.display = "block";
+  evt.currentTarget.className += " active";
+}
+
+window.onload = function () {
+  // Gọi hàm tabs với event và categoryName bằng với id của phần tử "defaultOpen"
+  document.getElementById("defaultOpen").click();
+};
