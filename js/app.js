672bd8bff63dad99c2caa0fad3cb1117ea20be7b
Chau Nhi <chaunhihue@gmail.com>
2023-07-19 12:26:02 +0700

    upload source code

---
+++ js/app.js
@@ -0,0 +1,112 @@
+import data from './data.js';
+
+
+console.log(data);
+const products = document.querySelector(".product .grid_row");
+// const new_products = document.querySelector(".body-make-up");
+// console.log(new_products);
+// console.log(products);
+
+// Format Price
+function formatPrice(price) {
+    const formatter = new Intl.NumberFormat('vi-VN', {
+      style: 'currency',
+      currency: 'VND'
+    });
+    if(price === "") return "";
+    else 
+        return formatter.format(price) ;
+}
+
+// Showing product
+function ham(name,category,img,newPrice,oldPrice) {
+    return `<div class="grid_column-3">
+                <a href="" class="home__product--item">
+                    <div class="home__product-item-img" style="background-image: url(${img});"></div>
+                    
+                    <h4 class="home__collection-item-heading">${name}</h4>
+                    <h4 class="home__collection-category">${category}</h4>
+                    <div class="home__collection-item-price">
+                        <span class="home__collection-item-currentPrice">${formatPrice(newPrice)}</span>
+                        <span class="home__collection-item-oldPrice">${formatPrice(oldPrice)}</span>
+                    </div>
+                    
+                    <div class="home__collection-action">
+                        <div class="home__collection-item__rating">
+                            <i class="home-collection-item__star-gold fa-solid fa-star"></i>
+                            <i class="home-collection-item__star-gold fa-solid fa-star"></i>
+                            <i class="home-collection-item__star-gold fa-solid fa-star"></i>
+                            <i class="home-collection-item__star-gold fa-solid fa-star"></i>
+                            <i class=" fa-solid fa-star"></i>
+                        </div>
+
+                        <i class="home__collection-item-navigate-icon fa-solid fa-arrow-up-long"></i>
+                        
+                    </div>
+
+                    
+                    <div href="" class="add-cart--link">
+                        <i class="add-cart--icon fa-solid fa-cart-shopping"></i>
+                    Add to cart
+                    </div>
+                </a>
+            </div>`
+
+
+
+}
+let currentArray = [];
+const title = (document.querySelector(".product__header-title")).textContent;
+
+
+if (title === "Men's shoes"){
+    const Shoes = data.filter((item) => item.category === "Men's shoes");
+    currentArray = Shoes;
+    const courses = Shoes.map((item) => ham(item.name,item.category,item.img,item.newPrice,item.oldPrice))
+    products.innerHTML = courses.join("");
+}
+
+
+if (title === "Women's shoes"){
+    const Shoes = data.filter((item) => item.category === "Women's shoes");
+    currentArray = Shoes;
+    const courses = Shoes.map((item) => ham(item.name,item.category,item.img,item.newPrice,item.oldPrice))
+    products.innerHTML = courses.join("");
+}
+
+if (title === "Kids' shoes"){
+    const Shoes = data.filter((item) => item.category === "Kids' shoes");
+    currentArray = Shoes;
+    const courses = Shoes.map((item) => ham(item.name,item.category,item.img,item.newPrice,item.oldPrice))
+    products.innerHTML = courses.join("");
+}
+
+
+//Sorting
+
+window.sortingPrice = function() {
+    let valueSelect = document.querySelector(".product__header-sort-btn").value;
+
+    currentArray.sort((a, b) => {
+        const collator = new Intl.Collator("vi-VN", {numeric: true});
+        if (valueSelect === "az") {
+            return collator.compare(a.newPrice, b.newPrice);
+        } else if (valueSelect === "za") {
+            return collator.compare(b.newPrice, a.newPrice);
+        } else {
+            return;
+        }
+    });
+    const co = currentArray.map((item) => ham(item.name, item.category, item.img, item.newPrice, item.oldPrice));
+    products.innerHTML = co.join("");
+}
+
+
+
+
+
+
+
+
+
+
--- js/data.js
+++ js/data.js
@@ -1,3 +1,411 @@
-// export data {
+export default [
+  // men
+  {
+    id: "m1",
+    name: "Nike Air Max 90",
+    category: "Men's shoes",
+    img: "https://static.nike.com/a/images/t_PDP_1728_v1/f_auto,q_auto:eco/3bbded7c-371f-4a16-ada3-4bfc6ae522df/air-max-90-shoes-kRsBnD.png",
+    newPrice: 3519000,
+    oldPrice: 3700000,
+  },
 
-// }
+  {
+    id: "m2",
+    name: "Nike Air Huarache Premium",
+    category: "Men's shoes",
+    img: "https://static.nike.com/a/images/t_PDP_1280_v1/f_auto,q_auto:eco/597481c6-05ea-4b1a-86c2-ff798c592863/air-huarache-shoes-lCV2vJ.png",
+    newPrice: 2465399,
+    oldPrice: 4109000,
+  },
+
+  {
+    id: "m3",
+    name: "Nike Air Max 95",
+    category: "Men's shoes",
+    img: "https://static.nike.com/a/images/t_PDP_1280_v1/f_auto,q_auto:eco/f411a3ec-39b7-490e-b2c8-335264adb054/air-max-95-shoes-CwsxnW.png",
+    newPrice: 3289299,
+    oldPrice: 4699000,
+  },
+
+  {
+    id: "m4",
+    name: "Nike E-Series 1.0",
+    category: "Men's shoes",
+    img: "https://static.nike.com/a/images/t_PDP_1728_v1/f_auto,q_auto:eco/da1b52f3-552c-405d-a789-5a8c1ed3f7fd/e-series-1-shoes-j57hxR.png",
+    newPrice: 2929000,
+    oldPrice: "",
+  },
+
+  {
+    id: "m5",
+    name: "Jordan Stadium 90",
+    category: "Men's shoes",
+    img: "https://static.nike.com/a/images/t_PDP_1728_v1/f_auto,q_auto:eco/edf63e23-4200-4d64-9c7f-239518907825/jordan-stadium-90-shoes-Jn6ZH4.png",
+    newPrice: 6500000,
+    oldPrice: "",
+  },
+
+  {
+    id: "m6",
+    name: "Nike Air Huarache Runner",
+    category: "Men's shoes",
+    img: "https://static.nike.com/a/images/t_PDP_1728_v1/f_auto,q_auto:eco/4800ce1e-512d-49f6-b977-ef78bb26c23b/air-max-systm-shoes-hLmQ85.png",
+    newPrice: 4109000,
+    oldPrice: "",
+  },
+
+  {
+    id: "m7",
+    name: "Nike Tech Hera",
+    category: "Men's shoes",
+    img: "https://static.nike.com/a/images/t_PDP_1728_v1/f_auto,q_auto:eco/774e0af9-109b-46a6-83c1-1c6c85d63609/tech-hera-shoes-8MQgCL.png",
+    newPrice: 3239000,
+    oldPrice: "",
+  },
+
+  {
+    id: "m8",
+    name: "Nike Air Force 1 '07",
+    category: "Men's shoes",
+    img: "https://static.nike.com/a/images/c_limit,w_592,f_auto/t_product_v1/8a811235-fbad-4343-8b18-09b8984bf3d2/air-force-1-07-shoes-JSrcPm.png",
+    newPrice: 3829000,
+    oldPrice: "",
+  },
+
+  {
+    id: "m9",
+    name: "Nike Pegasus 40 Premium",
+    category: "Men's shoes",
+    img: "https://static.nike.com/a/images/c_limit,w_592,f_auto/t_product_v1/f52549ad-24d9-4c23-98fa-7052c11e47bc/pegasus-40-road-running-shoes-50CtF7.png",
+    newPrice: 4109000,
+    oldPrice: "",
+  },
+
+  {
+    id: "m10",
+    name: "Nike Invincible 3",
+    category: "Men's shoes",
+    img: "https://static.nike.com/a/images/c_limit,w_592,f_auto/t_product_v1/e9f8c612-0b6b-4e4e-aaf8-af0d8b88b40c/invincible-3-road-running-shoes-Wwmmlp.png",
+    newPrice: 5279000,
+    oldPrice: "",
+  },
+
+  {
+    id: "m11",
+    name: "Nike ACG Mountain Fly 2 Low",
+    category: "Men's shoes",
+    img: "https://static.nike.com/a/images/c_limit,w_592,f_auto/t_product_v1/111d0ca2-3735-4968-8d5c-8bd790c95044/acg-mountain-fly-2-low-shoes-4LXRfM.png",
+    newPrice: 3450000,
+    oldPrice: 4320000,
+  },
+
+  {
+    id: "m12",
+    name: "Nike Blazer Low '77 Jumbo",
+    category: "Men's shoes",
+    img: "https://static.nike.com/a/images/c_limit,w_592,f_auto/t_product_v1/8d5a336c-ea74-4ed6-9fbf-6467ff671971/blazer-low-77-jumbo-shoes-rHhr2x.png",
+    newPrice: 3089000,
+    oldPrice: "",
+  },
+
+  {
+    id: "m13",
+    name: "Nike Revolution 6",
+    category: "Men's shoes",
+    img: "https://static.nike.com/a/images/c_limit,w_592,f_auto/t_product_v1/32e90900-2918-4715-a6c8-c2b9eb82d172/revolution-6-running-shoes-5k6Jh6.png",
+    newPrice: 1759000,
+    oldPrice: "",
+  },
+
+  {
+    id: "m14",
+    name: "Nike Dunk Low Retro",
+    category: "Men's shoes",
+    img: "https://static.nike.com/a/images/c_limit,w_592,f_auto/t_product_v1/4041618c-dd05-4e4d-ba2f-3bd699d67d2e/dunk-low-retro-shoes-69h36X.png",
+    newPrice: 2929000,
+    oldPrice: "",
+  },
+
+  {
+    id: "m15",
+    name: "Nike Downshifter 12",
+    category: "Men's shoes",
+    img: "https://static.nike.com/a/images/c_limit,w_592,f_auto/t_product_v1/bf0c5ddb-53bb-498e-932e-2fd0a0ff3a13/downshifter-12-road-running-shoes-kQLnZn.png",
+    newPrice: 1909000,
+    oldPrice: 4320000,
+  },
+
+  // women
+
+  {
+    id: "wm1",
+    name: "Nike Air Max 90 Futura",
+    category: "Women's shoes",
+    img: "https://static.nike.com/a/images/t_PDP_1728_v1/f_auto,q_auto:eco/87d7e901-4d08-4456-9d62-07e821aff45b/air-max-90-futura-shoes-x9msLm.png",
+    newPrice: 4109000,
+    oldPrice: 4320000,
+  },
+
+  {
+    id: "wm2",
+    name: "Nike Air Max Excee",
+    category: "Women's shoes",
+    img: "https://static.nike.com/a/images/t_PDP_1280_v1/f_auto,q_auto:eco/4ff2fe4a-4f74-4189-82f0-3ea780f9389d/air-max-excee-shoes-XXdkKj.png",
+    newPrice: 2251649,
+    oldPrice: 2649000,
+  },
+
+  {
+    id: "wm3",
+    name: "Nike Air Force 1 Shadow",
+    category: "Women's shoes",
+    img: "https://static.nike.com/a/images/t_PDP_1728_v1/f_auto,q_auto:eco/4621066a-6e19-49e5-a2d7-4e7e5eade298/air-force-1-shadow-shoes-mN8Glx.png",
+    newPrice: 3519000,
+    oldPrice: "",
+  },
+
+  {
+    id: "wm4",
+    name: "Nike Tech Hera",
+    category: "Women's shoes",
+    img: "https://static.nike.com/a/images/t_PDP_1728_v1/f_auto,q_auto:eco/8d6ec7db-5575-462f-804c-8694044f4b06/tech-hera-shoes-JlV5km.png",
+    newPrice: 3239000,
+    oldPrice: 2649000,
+  },
+
+  {
+    id: "wm5",
+    name: "Nike Court Legacy Lift",
+    category: "Women's shoes",
+    img: "https://static.nike.com/a/images/t_PDP_1728_v1/f_auto,q_auto:eco/10ae600e-0b18-4d11-920c-b8bcdb857eb3/court-legacy-lift-shoes-1zLxsK.png",
+    newPrice: 2499000,
+    oldPrice: "",
+  },
+
+  {
+    id: "wm6",
+    name: "Nike Air Huarache Craft",
+    category: "Women's shoes",
+    img: "https://static.nike.com/a/images/t_PDP_1728_v1/f_auto,q_auto:eco/179e23ab-dfae-4a3e-a295-fc2f44e98d4f/air-huarache-craft-shoes-Xf0QhN.png",
+    newPrice: 2876299,
+    oldPrice: 4109000,
+  },
+
+  {
+    id: "wm7",
+    name: "Nike Air Force 1 '07",
+    category: "Women's shoes",
+    img: "https://static.nike.com/a/images/t_PDP_1728_v1/f_auto,q_auto:eco/92c62463-2965-4aef-974b-3fd4f8807e4f/air-force-1-07-shoes-5ttFDf.png",
+    newPrice: 3239000,
+    oldPrice: "",
+  },
+
+  {
+    id: "wm8",
+    name: "Nike Air Force 1 PLT.AF.ORM LV8",
+    category: "Women's shoes",
+    img: "https://static.nike.com/a/images/t_PDP_1728_v1/f_auto,q_auto:eco/602188c3-be75-4aa0-aab0-5fa4b59e0927/air-force-1-pltaform-lv8-shoes-bQSqmJ.png",
+    newPrice: 2251649,
+    oldPrice: 2649000,
+  },
+
+  {
+    id: "wm9",
+    name: "Nike Air Force 1 Shadow",
+    category: "Women's shoes",
+    img: "https://static.nike.com/a/images/t_PDP_1728_v1/f_auto,q_auto:eco/e7f621f8-d62e-481c-9ca5-66c961d055df/air-force-1-shadow-shoes-3d774m.png",
+    newPrice: 3829000,
+    oldPrice: "",
+  },
+
+  {
+    id: "wm10",
+    name: "Air Jordan 1 Elevate Low",
+    category: "Women's shoes",
+    img: "https://static.nike.com/a/images/c_limit,w_592,f_auto/t_product_v1/b31587b0-3d2a-4a59-b02e-720d4d0b8973/air-jordan-1-elevate-low-shoes-XlkVrM.png",
+    newPrice: 3829000,
+    oldPrice: "",
+  },
+
+  {
+    id: "wm11",
+    name: "Nike React Infinity 3 Premium",
+    category: "Women's shoes",
+    img: "https://static.nike.com/a/images/t_PDP_1728_v1/f_auto,q_auto:eco/6d8eb010-3195-4411-9af0-dd1a11ed76ae/react-infinity-3-road-running-shoes-919kdc.png",
+    newPrice: 4999000,
+    oldPrice: "",
+  },
+
+  {
+    id: "wm12",
+    name: "Nike Air Force 1 '07 LV8 EMB",
+    category: "Women's shoes",
+    img: "https://static.nike.com/a/images/t_PDP_1728_v1/f_auto,q_auto:eco/7894dfa1-f942-4591-9272-3e2ddd66bc31/air-force-1-07-lv8-emb-shoes-rfzXL8.png",
+    newPrice: 2991149,
+    oldPrice: 3519000,
+  },
+
+  {
+    id: "wm13",
+    name: "Nike Winflo 10 Premium",
+    category: "Women's shoes",
+    img: "https://static.nike.com/a/images/c_limit,w_592,f_auto/t_product_v1/8939a2e3-6dcc-4b35-913a-355564b41bc7/wio-10-road-running-shoes-gT1lTR.png",
+    newPrice: 3239000,
+    oldPrice: "",
+  },
+
+  {
+    id: "wm14",
+    name: "Nike Vaporfly 3",
+    category: "Women's shoes",
+    img: "https://static.nike.com/a/images/t_PDP_1728_v1/f_auto,q_auto:eco/50b864ba-2ab4-4a45-823d-b3b53785b353/vaporfly-3-road-racing-shoes-wdmHPR.png",
+    newPrice: 6609000,
+    oldPrice: "",
+  },
+
+  {
+    id: "wm15",
+    name: "Nike Invincible 3",
+    category: "Women's shoes",
+    img: "https://static.nike.com/a/images/t_PDP_1728_v1/f_auto,q_auto:eco/0a883b6f-1a3c-42d7-943e-a2e4a4107b18/invincible-3-road-running-shoes-Wwmmlp.png",
+    newPrice: 5279000,
+    oldPrice: "",
+  },
+
+  // kid
+
+  {
+    id: "k1",
+    name: "Nike Flex Plus 2",
+    category: "Kids' shoes",
+    img: "https://static.nike.com/a/images/t_PDP_1728_v1/f_auto,q_auto:eco/45dfffb5-c5e1-4be5-a931-1a8bc166e721/flex-plus-2-older-running-shoes-PbFFJC.png",
+    newPrice: 1529000,
+    oldPrice: "",
+  },
+
+  {
+    id: "k2",
+    name: "Jordan Stadium 90",
+    category: "Kids' shoes",
+    img: "https://static.nike.com/a/images/t_PDP_1728_v1/f_auto,q_auto:eco/c1e323fd-acc5-4581-831c-4a1d31972906/jordan-stadium-90-older-shoes-2J5HrK.png",
+    newPrice: 2809000,
+    oldPrice: "",
+  },
+
+  {
+    id: "k3",
+    name: "Nike Air Force 1 LV8",
+    category: "Kids' shoes",
+    img: "https://static.nike.com/a/images/t_PDP_1728_v1/f_auto,q_auto:eco/9d3ce94e-9f0e-46d9-b1ec-2ef1069a41df/air-force-1-lv8-older-shoes-BWHxJx.png",
+    newPrice: 2549000,
+    oldPrice: 2949000,
+  },
+
+  {
+    id: "k4",
+    name: "Nike Air Force 1",
+    category: "Kids' shoes",
+    img: "https://static.nike.com/a/images/t_PDP_1728_v1/f_auto,q_auto:eco/b5aedc81-3e85-4bd2-9b77-d43aeb8faa7c/air-force-1-older-shoes-1GxH0J.png",
+    newPrice: 2267000,
+    oldPrice: 2650000,
+  },
+
+  {
+    id: "k5",
+    name: "Nike Pico 5",
+    category: "Kids' shoes",
+    img: "https://static.nike.com/a/images/t_PDP_1728_v1/f_auto,q_auto:eco/f9e7b076-da72-419c-aaf5-86c8a2785cbb/pico-5-shoes-QQ5g1N.png",
+    newPrice: 889000,
+    oldPrice: "",
+  },
+
+  {
+    id: "k6",
+    name: "Nike Air Max 90 LTR",
+    category: "Kids' shoes",
+    img: "https://static.nike.com/a/images/t_PDP_1728_v1/f_auto,q_auto:eco/20f900f7-8b99-4c0f-86b1-96945d1d8ee4/air-max-90-ltr-older-shoes-M0TsXt.png",
+    newPrice: 2549000,
+    oldPrice: "",
+  },
+
+  {
+    id: "k7",
+    name: "Nike Team Hustle D 11",
+    category: "Kids' shoes",
+    img: "https://static.nike.com/a/images/t_PDP_1728_v1/f_auto,q_auto:eco/fc7cb765-c53e-494b-85e0-9ea483a6cd03/team-hustle-d-11-younger-shoes-5FsXFk.png",
+    newPrice: 1989000,
+    oldPrice: 2649000,
+  },
+
+  {
+    id: "k8",
+    name: "Nike Dynamo Go",
+    category: "Kids' shoes",
+    img: "https://static.nike.com/a/images/t_PDP_1280_v1/f_auto,q_auto:eco/cc622c2f-b34c-45a0-975f-3e1135ea7f79/dynamo-go-younger-easy-on-off-shoes-dCf2Zv.png",
+    newPrice: 2251649,
+    oldPrice: "",
+  },
+
+  {
+    id: "k9",
+    name: "Nike Air Force 1 LV8",
+    category: "Kids' shoes",
+    img: "https://static.nike.com/a/images/t_PDP_1728_v1/f_auto,q_auto:eco/e230708f-45d0-491f-ac9a-2d1bc54a754a/air-force-1-lv8-older-shoes-dRQ3fJ.png",
+    newPrice: 2419000,
+    oldPrice: "",
+  },
+
+  {
+    id: "k10",
+    name: "Air Jordan 1 Low SE",
+    category: "Kids' shoes",
+    img: "https://static.nike.com/a/images/t_PDP_1728_v1/f_auto,q_auto:eco/9e7bc064-d3b7-4f6a-b8d4-2d108e7837a0/air-jordan-1-low-se-older-shoes-kg1hPR.png",
+    newPrice: 2387649,
+    oldPrice: 2389000,
+  },
+
+  {
+    id: "k11",
+    name: "Nike Air Zoom Arcadia 2",
+    category: "Kids' shoes",
+    img: "https://static.nike.com/a/images/t_PDP_1728_v1/f_auto,q_auto:eco/ffcac9a3-4972-4277-bc7a-047c80b9d988/air-zoom-arcadia-2-older-shoes-Mb9l9m.png",
+    newPrice: 1909000,
+    oldPrice: "",
+  },
+
+  {
+    id: "k12",
+    name: "Sky Jordan 1",
+    category: "Kids' shoes",
+    img: "https://static.nike.com/a/images/t_PDP_1728_v1/f_auto,q_auto:eco/6b302885-29f2-4cdf-8ef3-fee6d57dd62b/sky-jordan-1-younger-shoe-sWmN77.png",
+    newPrice: 1789000,
+    oldPrice: "",
+  },
+
+  {
+    id: "k13",
+    name: "Nike Air More Uptempo",
+    category: "Kids' shoes",
+    img: "https://static.nike.com/a/images/t_PDP_1728_v1/f_auto,q_auto:eco/aba51b43-a8f9-462d-b639-c2abc1fe1356/air-more-uptempo-younger-shoes-CgqzW1.png",
+    newPrice: 2499000,
+    oldPrice: 2649000,
+  },
+
+  {
+    id: "k14",
+    name: "LeBron Witness 7",
+    category: "Kids' shoes",
+    img: "https://static.nike.com/a/images/t_PDP_1728_v1/f_auto,q_auto:eco/38b3aa01-5cd0-4518-b2ee-b8e350346a42/lebron-witness-7-younger-shoes-QDJM6D.png",
+    newPrice: 2039000,
+    oldPrice: "",
+  },
+
+  {
+    id: "k15",
+    name: "Nike Force 1 LV8 3",
+    category: "Kids' shoes",
+    img: "https://static.nike.com/a/images/t_PDP_1728_v1/f_auto,q_auto:eco/i1-4d1de18e-d4d5-46b7-ad02-a46d5656e659/force-1-lv8-3-younger-shoes-ctV5K6.png",
+    newPrice: 1733149,
+    oldPrice: 2039000,
+  },
+];
